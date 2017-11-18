5499dea9bd5d14e29443307d93263b262a460ebf
Douman <douman@gmx.se>
2017-11-18 22:08:07 +0300

    0.2: Better clipboard setter and new settings page

--- auto-clip.js
+++ auto-clip.js
@@ -1,34 +1,46 @@
-'use strict';
+'use strict'
 
 function init(settings) {
     //In case of array... cuz firefox
-    if (settings.length) settings = settings[0];
+    if (settings.length) settings = settings[0]
 
     browser.storage.onChanged.addListener(function(changes, area) {
         Object.keys(changes).forEach(function(key) {
-            settings[key] = changes[key].newValue;
-        });
-    });
+            settings[key] = changes[key].newValue
+        })
+    })
 
     function clean_selection_if() {
         if (settings.select_clean) {
-            if (window.getSelection) window.getSelection().removeAllRanges();
-            else if (document.selection) document.selection.empty();
+            if (window.getSelection) window.getSelection().removeAllRanges()
+            else if (document.selection) document.selection.empty()
         }
     }
 
+    function set_clipboard(text, mime) {
+        const setClipboardData = evt => {
+            document.removeEventListener("copy", setClipboardData, true)
+            evt.stopImmediatePropagation()
+            evt.preventDefault()
+            evt.clipboardData.setData(mime, text)
+        }
+
+        document.addEventListener("copy", setClipboardData, true)
+        document.execCommand("copy")
+    }
+
     document.addEventListener("mouseup", function() {
-        var selected_text = window.getSelection().toString().trim();
+        var selected_text = settings.select_trim === true ? window.getSelection().toString().trim() : window.getSelection().toString()
 
         if (selected_text) {
-            document.execCommand("Copy");
-            clean_selection_if();
+            set_clipboard(selected_text, "text/plain")
+            clean_selection_if()
         }
-    });
+    })
 }
 
 function on_error(error) {
-    console.log(`Error: ${error}`);
+    console.log(`Error: ${error}`)
 }
 
-browser.storage.local.get(null).then(init, on_error);
+browser.storage.local.get(null).then(init, on_error)
--- options/index.js
+++ options/index.js
@@ -1,32 +1,33 @@
-'use strict';
+'use strict'
 
 function set_option(event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    var setting = {};
+    var setting = {}
 
-    if (event.target.type === "checkbox") setting[event.target.id] = event.target.checked;
+    if (event.target.type === "checkbox") setting[event.target.id] = event.target.checked
 
-    browser.storage.local.set(setting);
+    browser.storage.local.set(setting)
 }
 
 function init() {
-    var settings = document.querySelectorAll("form > * input");
+    var settings = document.querySelectorAll("form > input")
 
     for (var idx = 0; idx < settings.length; idx += 1) {
-        settings[idx].addEventListener("change", set_option);
+        settings[idx].addEventListener("change", set_option)
     }
 
     function set(result) {
-        document.getElementById("select_clean").checked = result["select_clean"] || false;
+        document.getElementById("select_clean").checked = result["select_clean"] || false
+        document.getElementById("select_trim").checked = result["select_trim"] || false
     }
 
     function on_error(error) {
-        console.log(`Error: ${error}`);
+        console.log(`Error: ${error}`)
     }
 
-    browser.storage.local.get(null).then(set, on_error);
+    browser.storage.local.get(null).then(set, on_error)
 }
 
 
-document.addEventListener("DOMContentLoaded", init);
+document.addEventListener("DOMContentLoaded", init)
